496b4d102d0607c9f836447ad8410a81af5ba622
Arkadiusz Nowakowski <arkadiusz.nowakowski@us.edu.pl>
2019-03-25 22:44:52 +0100

    Add the jump animation at random

--- Assets/Scripts/MugScript.cs
+++ Assets/Scripts/MugScript.cs
@@ -4,8 +4,33 @@ using UnityEngine;
 
 public class MugScript : MonoBehaviour
 {
+    Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        StartCoroutine("RandomlyAnim");
+    }
+
     void OnMouseDown()
     {
-        
+        AnimatorStateInfo animatorStateInfo = animator.GetCurrentAnimatorStateInfo(0);
+        if (animatorStateInfo.IsName("Idle"))
+        {
+            Debug.Log("The mug has been clicked");
+        }
+    }
+
+    IEnumerator RandomlyAnim()
+    {
+        for (; ; )
+        {
+            yield return new WaitForSeconds(Random.Range(4.0f, 12.0f));
+            AnimatorStateInfo animatorStateInfo = animator.GetCurrentAnimatorStateInfo(0);
+            if (animatorStateInfo.IsName("Idle"))
+            {
+                animator.SetTrigger("Jump");
+            }
+        }
     }
 }
